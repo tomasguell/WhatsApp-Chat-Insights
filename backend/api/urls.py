cb8eb2f42b241042a46e3272f10d6c0ea9b55a31
tomasguell <tomasguell123@gmail.com>
2023-05-14 16:24:55 -0300

    api views detalle sender

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -5,4 +5,10 @@ urlpatterns = [
     path("chats/<str:pk>/content", views.ChatContent, name="chatContent"),
     path("chats/<str:pk>/stats", views.ChatStats, name="chatStats"),
     path("chats", views.Chats, name="chats"),
+    path("sender/<str:pks>/content", views.SenderContent, name="senderContent"),
+    path(
+        "sender/<str:pks>/content/<str:pkc>",
+        views.SenderContentChat,
+        name="senderContentChat",  # pks (s de sender) (c de chat)
+    ),
 ]
--- backend/api/views.py
+++ backend/api/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from rest_framework.response import Response
 from .funcs import leer_chat_whatsapp
-from .models import Chat, Message
+from .models import Chat, Message, Sender
 from rest_framework.decorators import api_view
 from .serializers import ChatSerializer, MessageSerializer
 
@@ -26,6 +26,36 @@ def ChatStats(request, pk):
         return Response(
             response_data
         )  # Quiero que returnee el numero de mensajes del chat
+
+
+@api_view(["GET", "POST"])
+def SenderContent(request, pks):  # pks (s de sender)
+    if request.method == "GET":
+        messages = Message.objects.all().filter(Sender__id=pks)
+
+        serializer = MessageSerializer(messages, many=True)
+        data = serializer.data
+        response_data = {"sender": Sender.objects.get(id=pks).Name, "content": data}
+
+        return Response(
+            response_data
+        )  # Quiero que returnee el numero de mensajes del chat
+
+
+@api_view(["GET", "POST"])
+def SenderContentChat(request, pks, pkc):  # pks (s de sender)
+    if request.method == "GET":
+        messages = Message.objects.all().filter(Sender__id=pks, Chat__id=pkc)
+
+        serializer = MessageSerializer(messages, many=True)
+        data = serializer.data
+        response_data = {
+            "sender": Sender.objects.get(id=pks).Name,
+            "chat": Chat.objects.get(id=pkc).Title,
+            "content": data,
+        }
+
+        return Response(response_data)
 
 
 @api_view(["GET", "POST"])
